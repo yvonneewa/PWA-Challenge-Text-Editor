e6f62cb459e4e05456abb95af1798fd661e1a5b9
Yvonne <yowantings@gmail.com>
2024-07-06 12:29:57 -0600

    added code to the src js

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,20 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) =>
+    request.destination === 'script' ||
+    request.destination === 'style' ||
+    request.destination === 'image',
+  new CacheFirst({
+    cacheName: 'assets-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 7 * 24 * 60 * 60, 
+      }),
+    ],
+  })
+);
