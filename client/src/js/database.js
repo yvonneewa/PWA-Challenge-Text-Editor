439935ba7f0a5eb3bec23ec47dc6625e13e35bda
Yvonne <yowantings@gmail.com>
2024-07-06 12:19:36 -0600

    npm i and installed package lock

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,33 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) =>{
+
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  await store.add(content);
+  await tx.done;
+  console.error('putDb not implemented');
+
+
+}
+
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () =>{
+
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const allContent = await store.getAll();
+  await tx.done;
+  console.error('getDb not implemented');
+  return allContent;
+}
+  
+  
+
 
 initdb();
