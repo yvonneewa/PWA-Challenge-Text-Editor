af232de5c2092f9f5e61678b1345c0eadfe28e76
Yvonne <yowantings@gmail.com>
2024-07-05 20:35:52 -0600

    made changes in install and webpack js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,30 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+windowdeferredPrompt = event;
+butInstall.classList.toggle('hidden',false);
+
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+
+    const promptEvent = windowdeferredPrompt;
+
+    if (!promptEvent){
+        return;
+    }
+
+    promptEvent.prompt();
+
+    windowdeferredPrompt = null;
+
+    butInstall.classList.toggle('hiden', true)
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+
+    window.defferedPrompt = null
+});
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,18 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        title: 'pwa editor',
+        template: './index.html',
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
       ],
     },
   };
